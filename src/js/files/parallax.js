ab3385b817e53049b06a1198037d95429cadb160
lay_morja <evil-corporaty@yandex.ru>
2023-08-06 13:42:46 +0300

    Return to default parallax init

--- src/js/files/parallax.js
+++ src/js/files/parallax.js
@@ -2,6 +2,5 @@ import SimpleParallax from 'simple-parallax-js';
 
 const parallaxImages = document.querySelectorAll('.parallax-image');
 new SimpleParallax(parallaxImages, {
-  scale: 1.5,
   delay: 0.6,
 });
