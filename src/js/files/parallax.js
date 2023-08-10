961428cd4e46d30d6db3063ce232d9033443ac72
lay_morja <evil-corporaty@yandex.ru>
2023-08-10 22:43:57 +0300

    Change parallax scale

--- src/js/files/parallax.js
+++ src/js/files/parallax.js
@@ -3,5 +3,5 @@ import SimpleParallax from 'simple-parallax-js';
 const parallaxImages = document.querySelectorAll('.parallax-image');
 new SimpleParallax(parallaxImages, {
   delay: 0.6,
-  scale: 1.1,
+  scale: 1.3,
 });
