40686e786e44481e446d40dbfd7125ee257316c6
lay_morja <evil-corporaty@yandex.ru>
2023-08-10 22:38:16 +0300

    Remove menu function

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -10,40 +10,40 @@ import { flsModules } from './modules.js';
 // Подключение списка активных модулей
 // import { flsModules } from "./modules.js";
 
-if (isMobile.any()) {
-  document.documentElement.classList.add('mobile');
+// if (isMobile.any()) {
+//   document.documentElement.classList.add('mobile');
 
-  (function closeMenu() {
-    let x1 = null;
-    let y1 = null;
-    let isActive = false;
+//   (function closeMenu() {
+//     let x1 = null;
+//     let y1 = null;
+//     let isActive = false;
 
-    const handleTouchStart = function (e) {
-      if (e.target.closest('.menu')) {
-        isActive = true;
-      }
-      x1 = e.touches[0].clientX;
-      y1 = e.touches[0].clientY;
-    };
+//     const handleTouchStart = function (e) {
+//       if (e.target.closest('.menu')) {
+//         isActive = true;
+//       }
+//       x1 = e.touches[0].clientX;
+//       y1 = e.touches[0].clientY;
+//     };
 
-    const handleTouchMove = function (e) {
-      if (!isActive || !x1 || !y1) return;
-      let x2 = e.touches[0].clientX;
-      let y2 = e.touches[0].clientY;
+//     const handleTouchMove = function (e) {
+//       if (!isActive || !x1 || !y1) return;
+//       let x2 = e.touches[0].clientX;
+//       let y2 = e.touches[0].clientY;
 
-      let xDiff = x2 - x1;
-      let yDiff = y2 - y1;
+//       let xDiff = x2 - x1;
+//       let yDiff = y2 - y1;
 
-      if (Math.abs(xDiff) > Math.abs(yDiff) && xDiff > 0) menuClose();
-      x1 = null;
-      y1 = null;
-      isActive = false;
-    };
+//       if (Math.abs(xDiff) > Math.abs(yDiff) && xDiff > 0) menuClose();
+//       x1 = null;
+//       y1 = null;
+//       isActive = false;
+//     };
 
-    document.addEventListener('touchstart', handleTouchStart, false);
-    document.addEventListener('touchmove', handleTouchMove, false);
-  })();
-}
+//     document.addEventListener('touchstart', handleTouchStart, false);
+//     document.addEventListener('touchmove', handleTouchMove, false);
+//   })();
+// }
 
 const parentDir = document.querySelector('.parent-dir').textContent;
 
